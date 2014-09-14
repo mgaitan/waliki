52b1cef10928efca508bc88aac6de6a59039cd8c
Martín Gaitán <gaitan@gmail.com>
2014-09-14 04:40:44 -0300

    diff view

--- waliki/git/views.py
+++ waliki/git/views.py
@@ -17,6 +17,7 @@ def history(request, slug):
                                                    'history': history,
                                                    'max_changes': max_changes})
 
+
 def version(request, slug, version):
     page = get_object_or_404(Page, slug=slug)
     content = Git().version(page, version)
@@ -31,6 +32,14 @@ def version(request, slug, version):
                                                    'version': version,
                                                    'form': form})
 
+
 def diff(request, slug, old, new):
     page = get_object_or_404(Page, slug=slug)
-    import ipdb; ipdb.set_trace()
+    old_content = Git().version(page, old)
+    new_content = Git().version(page, new)
+    return render(request, 'waliki/diff.html', {'page': page,
+                                                   'old_content': old_content,
+                                                   'new_content': new_content,
+                                                   'slug': slug,
+                                                   'old_commit': old,
+                                                   'new_commit': new})
