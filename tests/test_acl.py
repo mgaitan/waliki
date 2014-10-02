aacc7ab761a85ca394653926da8e37d72e71536f
Martín Gaitán <gaitan@gmail.com>
2014-10-01 23:38:48 -0300

    templatetag to check permission

--- tests/test_acl.py
+++ tests/test_acl.py
@@ -1,6 +1,7 @@
 from .factories import UserFactory, GroupFactory, ACLRuleFactory
 from waliki.models import ACLRule
 from django.test import TestCase
+from django.template import Template, Context, TemplateSyntaxError
 
 
 class TestGetUsersRules(TestCase):
@@ -42,7 +43,48 @@ class TestGetUsersRules(TestCase):
     def test_simple_user_for_multiples_perms(self):
         user1 = UserFactory()
         user2 = UserFactory()
-        ACLRuleFactory(slug='page', permissions=['view_page'], users=[user1, user2])
+        ACLRuleFactory(
+            slug='page', permissions=['view_page'], users=[user1, user2])
         ACLRuleFactory(slug='page', permissions=['change_page'], users=[user1])
         users = ACLRule.get_users_for(['view_page', 'change_page'], 'page')
         self.assertEqual(set(users), {user1})
+
+
+class CheckPermTagTest(TestCase):
+
+    def render_template(self, template, context):
+        """
+        Returns rendered ``template`` with ``context``, which are given as string
+        and dict respectively.
+        """
+        t = Template(template)
+        return t.render(Context(context))
+
+    def test_wrong_formats(self):
+        wrong_formats = (
+            # no quotes
+            '{% check_perms "view_page" for user in slug as has_perm %}',
+            # wrong quotes
+            '{% check_perms "view_page" for user in slug as \'has_perms" %}',
+            # wrong quotes
+            '{% check_perms view_page for user in slug as "has_perms" %}',
+            # wrong quotes
+            '{% check_perms "view_page, change_page for user in slug as "has_perms" %}',
+            # wrong quotes
+            '{% check_perms "view_page" user in slug as "has_perms" %}',
+            # no context_var
+            '{% check_perms "view_page" for user in slug as %}',
+            # no slug
+            '{% check_perms "view_page" for user as "has_perms" %}',
+            # no user
+            '{% check_perms "view_page" in slug as "has_perms" %}',
+            # no "for" bit
+            '{% check_perms "view_page, change_page" user in slug as "has_perms" %}',
+            # no "as" bit
+            '{% check_perms "view_page" for user in slug "has_perms" %}',
+        )
+        context = {'user': UserFactory(), 'slug': "any/slug"}
+        for wrong in wrong_formats:
+            fullwrong = '{% load waliki_tags %}' + wrong
+            with self.assertRaises(TemplateSyntaxError):
+                self.render_template(fullwrong, context)
--- waliki/templatetags/waliki_tags.py
+++ waliki/templatetags/waliki_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from waliki.acl import check_perms as check_perms_helper
 
 register = template.Library()
 
@@ -24,3 +25,62 @@ def get_attr(obj, val):
             return obj[val]
         except (KeyError, TypeError):
             return None
+
+
+class CheckPermissionsNode(template.Node):
+    def __init__(self, perms, user, slug, context_var):
+        self.perms = template.Variable(perms)
+        self.user = template.Variable(user)
+        self.slug = template.Variable(slug)
+        self.context_var = context_var
+
+    def render(self, context):
+        perms = [perm.strip() for perm in self.perms.var.split(',')]
+        user = self.user.resolve(context)
+        slug = self.slug.resolve(context)
+        context[self.context_var] = check_perms_helper(perms, user, slug)
+        return ''
+
+
+@register.tag
+def check_perms(parser, token):
+    """
+    Returns a list of permissions (as ``codename`` strings) for a given
+    ``user``/``group`` and ``obj`` (Model instance).
+
+    Parses ``check_perms`` tag which should be in format::
+
+        {% check_perms "perm1[, perm2, ...]" for user in slug as "context_var" %}
+
+    .. note::
+       Make sure that you set and use those permissions in same template
+       block (``{% block %}``).
+
+    Example of usage (assuming ``page` objects are
+    available from *context*)::
+
+        {% check_perms "delete_page" for request.user in page.slug as "can_delete" %}
+
+        {% if can_delete %}
+            ...
+        {% endif %}
+
+    """
+    bits = token.split_contents()
+    format = '{% check_perms "perm1[, perm2, ...]" for user in slug as "context_var" %}'
+    if len(bits) != 8 or bits[2] != 'for' or bits[4] != "in" or bits[6] != 'as':
+        raise template.TemplateSyntaxError("get_obj_perms tag should be in "
+                                           "format: %s" % format)
+    perms = bits[1]
+    user = bits[3]
+    slug = bits[5]
+    context_var = bits[7]
+    if perms[0] != perms[-1] or perms[0] not in ('"', "'"):
+        raise template.TemplateSyntaxError("check_perms tag's perms "
+                                           "argument should be in quotes")
+
+    if context_var[0] != context_var[-1] or context_var[0] not in ('"', "'"):
+        raise template.TemplateSyntaxError("check_perms tag's context_var "
+                                           "argument should be in quotes")
+    context_var = context_var[1:-1]
+    return CheckPermissionsNode(perms, user, slug, context_var)
