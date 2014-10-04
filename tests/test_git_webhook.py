4341d406897ab3e79ea1e8972d172556b2c2bc6a
Martín Gaitán <gaitan@gmail.com>
2014-10-03 21:34:44 -0300

    tests0

---
+++ tests/test_git_webhook.py
@@ -0,0 +1,82 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from .factories import PageFactory
+from waliki.settings import WALIKI_DATA_DIR
+from waliki.models import Page
+from waliki.git import Git
+import json
+import os
+import shutil
+from sh import git
+
+
+class TestGitWebhook(TestCase):
+
+    def setUp(self):
+        self.content = "- item"
+        self.page = PageFactory(path="page.rst", raw=self.content)
+        Git().commit(self.page)
+        self.remote = self.set_remote(True)
+        self.url = reverse('waliki_webhook_pull', args=('origin',))
+
+    def set_remote(self, chdir=False):
+        # init
+        remote = WALIKI_DATA_DIR + '_remote'
+        git.clone(WALIKI_DATA_DIR, remote)
+        os.chdir(WALIKI_DATA_DIR)
+        git.remote('add', 'origin', remote)
+        if chdir:
+            os.chdir(remote)
+        return remote
+
+    def tearDown(self):
+        try:
+            shutil.rmtree(WALIKI_DATA_DIR)
+            shutil.rmtree(self.remote)
+        except FileNotFoundError:
+            pass
+
+    def test_edit_file_no_conflict(self):
+        # commit remotely
+        with open("page.rst", 'w') as p:
+            p.write(self.content + '\nhey! edited remotely!\n')
+        git.add('.')
+        git.commit('-m', 'a remote log')
+        # webhook post (this would be done externally)
+        response = self.client.post(self.url, {})
+
+        self.assertEqual(self.page.raw, self.content + '\nhey! edited remotely!\n')
+        pull_stdout = json.loads(response.content.decode('utf8'))['pull']
+        self.assertIn('1 file changed', pull_stdout)
+
+    def test_edit_file_with_a_conflict(self):
+        # commit remotely
+        with open("page.rst", 'w') as p:
+            p.write(self.content + '\nremote line')
+        git.add('.')
+        git.commit('-m', 'a remote log')
+
+        # meanwhile. commit a change
+        self.page.raw = self.page.raw + '\nlocal line'
+        Git().commit(self.page)
+        response = self.client.post(self.url, {})
+        # local wins
+        self.assertEqual(self.page.raw, self.content + '\nlocal line\n')
+
+    def test_create_page_if_remote_added_files(self):
+        assert not Page.objects.filter(path="newpage.rst").exists()
+        with open("newpage.rst", 'w') as p:
+            p.write('the new content')
+        git.add('.')
+        git.commit('-m', 'add new page')
+        response = self.client.post(self.url, {})
+        self.assertEqual(Page.objects.count(), 2)
+        new_page = Page.objects.get(path="newpage.rst")
+        self.assertEqual(new_page.raw, 'the new content')
+
+    def test_remote_delete_a_page(self):
+        git.rm(self.page.path)
+        git.commit('-m', 'delete page')
+        response = self.client.post(self.url, {})
+        self.assertEqual(Page.objects.count(), 0)
+
