6b546b158848527d24f6965d9cbcf722ce86dc8a
Martín Gaitán <gaitan@gmail.com>
2014-11-11 12:02:34 -0300

    add migrate compat

--- waliki/migrations/0003_auto_20141110_0052.py
+++ waliki/migrations/0003_auto_20141110_0052.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
             model_name='aclrule',
             name='apply_to',
             field=models.CharField(choices=[('any', 'Any user'), ('logged', 'Any authenticated user'), ('staff', 'Any staff member'), ('superusers', 'Any superuser'), ('explicit', 'Any user/group explicitly defined')], default='explicit', max_length=25),
-            preserve_default=True,
+            # preserve_default=True,
         ),
     ]
