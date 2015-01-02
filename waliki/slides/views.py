993c179a37f32d42a16e1f3942e56f7617837501
Logan Chien <tzuhsiang.chien@gmail.com>
2015-01-02 23:35:22 +0800

    Cleanup temorary files and close the file.
    Cleanup the temporary files generated by hovercraft and
    close the file explicitly after reading content of the
    generated index.html.

--- waliki/slides/views.py
+++ waliki/slides/views.py
@@ -1,4 +1,5 @@
 from os import path
+import shutil
 import tempfile
 from sh import hovercraft
 from django.shortcuts import get_object_or_404
@@ -9,7 +10,12 @@ from waliki.models import Page
 def slides(request, slug):
     page = get_object_or_404(Page, slug=slug)
     outpath = tempfile.mkdtemp()
-    infile = page.abspath
-    template = path.join(path.dirname(path.realpath(__file__)), 'template')
-    hovercraft('-t', template, infile, outpath)
-    return HttpResponse(open(path.join(outpath, 'index.html')).read())
+    try:
+        infile = page.abspath
+        template = path.join(path.dirname(path.realpath(__file__)), 'template')
+        hovercraft('-t', template, infile, outpath)
+        with open(path.join(outpath, 'index.html')) as f:
+            content = f.read()
+    finally:
+        shutil.rmtree(outpath)
+    return HttpResponse(content)
