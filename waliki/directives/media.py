b4bb3824a95bedb4bbd11c14200b20a666167378
Martín Gaitán <gaitan@gmail.com>
2015-03-29 22:23:06 -0300

    extend rst with directives Added. .. media::

--- waliki/directives/media.py
+++ waliki/directives/media.py
@@ -7,12 +7,6 @@ except ImportError:
     micawber = None  # NOQA
 
 
-def register_directive():
-    if micawber is None:
-        return
-    directives.register_directive('media', Media)
-
-
 class Media(Directive):
     """ Restructured text extension for inserting any
         sort of media using micawber."""
@@ -22,14 +16,15 @@ class Media(Directive):
 
     def run(self):
         if micawber is None:
-            msg = "To use the media directive, isntall micawber first."
+            msg = "To use the media directive, install micawber first."
             return [nodes.raw('', '<div class="text-error">{0}</div>'.format(msg), format='html')]
 
         url = " ".join(self.arguments)
         providers = micawber.bootstrap_basic()
         data = providers.request(url)
-        html = '<h3>{}</h3>{}'.format(
-            data['title'],
-            micawber.parse_text(url, providers)
-        )
+        html = micawber.parse_text(url, providers)
         return [nodes.raw('', html, format='html')]
+
+
+def register_directive():
+    directives.register_directive('media', Media)
