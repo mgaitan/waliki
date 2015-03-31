7b1049a7889109b6c03c20b056412b4a2820fcc1
Martín Gaitán <gaitan@gmail.com>
2015-03-30 21:07:55 -0300

    simplifying media directive

--- waliki/directives/media.py
+++ waliki/directives/media.py
@@ -14,15 +14,15 @@ class Media(Directive):
     required_arguments = 1
     optional_arguments = 999
 
+    def get_providers(self):
+        return micawber.bootstrap_basic()
+
     def run(self):
         if micawber is None:
             msg = "To use the media directive, install micawber first."
             return [nodes.raw('', '<div class="text-error">{0}</div>'.format(msg), format='html')]
-
         url = " ".join(self.arguments)
-        providers = micawber.bootstrap_basic()
-        data = providers.request(url)
-        html = micawber.parse_text(url, providers)
+        html = micawber.parse_text(url, self.get_providers())
         return [nodes.raw('', html, format='html')]
 
 
