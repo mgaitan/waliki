18423d7f76c3a25ebc839860a7abb7c88e9db400
Martin Gaitan <mgaitan@onapsis.com>
2017-03-26 17:15:14 -0300

    fix project compat with django 1.10

--- waliki_project/waliki_project/urls.py
+++ waliki_project/waliki_project/urls.py
@@ -1,12 +1,24 @@
-from django.conf.urls import patterns, include, url
+
+try:
+    from django.conf.urls import patterns, include, url
+except ImportError:
+    patterns = None
+    from django.conf.urls import url, include
+
 from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # url(r'^$', 'waliki_project.views.home', name='home'),
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^boxes-example/', TemplateView.as_view(template_name="boxes_example.html")),
     url(r'^', include('waliki.urls')),
-)
+]
+
+if patterns:
+
+    urlpatterns = patterns('',
+        # url(r'^$', 'waliki_project.views.home', name='home'),
+        *urlpatterns
+    )
